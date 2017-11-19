5a52f43522d18c16af3e60d863b7b95a4ae4ae4c
shenty413 <shenty413@gmail.com>
2017-11-19 11:49:30 -0500

    test xcode project

---
+++ testproject/testprojectTests/testprojectTests.m
@@ -0,0 +1,39 @@
+//
+//  testprojectTests.m
+//  testprojectTests
+//
+//  Created by Tianyi Shen on 19/11/2017.
+//  Copyright © 2017 Tianyi Shen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testprojectTests : XCTestCase
+
+@end
+
+@implementation testprojectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
