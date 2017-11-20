f36be1191fb2c01c83e473f0bf0a6ff4304dfe10
张文远 <zhangwenyuan@zhangweuandeMBP.wv.cc.cmu.edu>
2017-11-19 22:58:10 -0500

    fixed bug

---
+++ detailed/header.h
@@ -0,0 +1,13 @@
+//
+//  header.h
+//  GatesFighter_Xcode
+//
+//  Created by 张文远 on 19/11/2017.
+//  Copyright © 2017 A1anzvvy. All rights reserved.
+//
+
+#ifndef header_h
+#define header_h
+
+
+#endif /* header_h */
