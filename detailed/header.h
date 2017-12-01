fe3119f113341f946ee93c1d2203f4a3fc04496f
shenty413 <shenty413@gmail.com>
2017-11-30 21:43:47 -0500

    change size of the character

--- detailed/header.h
+++ detailed/header.h
@@ -6,13 +6,13 @@ const double co = 3.14 / 180.;
 const double g = 2000.;
 
 //character parameter
-const double headr = 70.;
-const double neckl = 30.;
-const double bodyl = 200.;
-const double shoulderl = 100.;
-const double arml = 100;
-const double legl = 40;
-const double punchl = 40;
-const double omiga = 9.;
+const double headr = 70.*0.5;
+const double neckl = 30.*0.5;
+const double bodyl = 200.*0.5;
+const double shoulderl = 100.*0.5;
+const double arml = 100*0.5;
+const double legl = 40*0.5;
+const double punchl = 40*0.5;
+const double omiga = 9.*0.5;
 
 #endif /* header_h */
